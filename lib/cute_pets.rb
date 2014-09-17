9330de71b7b4a19077e1a4317c1135889e09d6e6
Erica Kwan <erica@codeforamerica.org>
2014-09-16 17:58:49 -0700

    Do the pet info fetch and tweet it

--- lib/cute_pets.rb
+++ lib/cute_pets.rb
@@ -1,9 +1,21 @@
-require 'net/http'
-require 'json'
-require 'open-uri'
+require 'cute_pets/pet_fetcher'
+require 'cute_pets/tweet_generator'
 require 'dotenv'
 Dotenv.load
 
 module CutePets
-  
+  extend self
+
+  def post_pet()
+    pet = nil
+    if ENV.fetch('pet_datasource').downcase == 'petfinder'
+      pet = PetFetcher.get_petfinder_pet
+    else
+      pet = PetFetcher.get_petharbor_pet
+    end
+    if pet
+      message = TweetGenerator.create_message(pet[:name], pet[:description], pet[:link])
+      TweetGenerator.tweet(message, pet[:pic])
+    end
+  end
 end
--- spec/cute_pets_spec.rb
+++ spec/cute_pets_spec.rb
@@ -1,6 +1,34 @@
 require 'cute_pets'
 require 'minitest/autorun'
 
+# Couldn't find a way to effectively mock modules via minitest :(
 describe 'CutePets' do
+  describe '.post_pet' do
+    before do
+      @pet_hash = { name:        'schooples',
+                   link:         'http://www.example.com/schooples',
+                   pic:          'http://www.example.com/schooples.jpg',
+                   description:  'neutured female fluffy dog'
+                 }
+    end
+    it 'fetches pet finder data when the env var datasource is set to petfinder' do
+      ENV.stub :fetch, 'petfinder' do
+        PetFetcher.stub(:get_petfinder_pet, @pet_hash) do
+          TweetGenerator.stub(:tweet, nil, [String, String]) do
+            CutePets.post_pet
+          end
+        end
+      end
+    end
 
+    it 'fetches pet harbor data when the env var datasource is set to petharbor' do
+      ENV.stub :fetch, 'petharbor' do
+        PetFetcher.stub(:get_petharbor_pet, @pet_hash) do
+          TweetGenerator.stub(:tweet, nil, [String, String]) do
+            CutePets.post_pet
+          end
+        end
+      end
+    end
+  end
 end
