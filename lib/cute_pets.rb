e5152583cebcf5a8482b9acc085067a5a03ae457
Matthew Leeds <mwl458@gmail.com>
2015-09-10 21:38:50 +0000

    Don't tweet pets we've already tweeted.

--- lib/cute_pets.rb
+++ lib/cute_pets.rb
@@ -6,14 +6,40 @@ Dotenv.load
 module CutePets
   extend self
 
-  def post_pet()
-    pet = nil
-    if ENV.fetch('pet_datasource').downcase == 'petfinder'
-      pet = PetFetcher.get_petfinder_pet
+  def fetch_pet
+    if ENV.fetch("pet_datasource").downcase == "petfinder"
+      PetFetcher.get_petfinder_pet
     else
-      pet = PetFetcher.get_petharbor_pet
+      PetFetcher.get_petharbor_pet
     end
+  end
+
+  def post_pet
+    pet = nil
+    pet = fetch_pet
+    # Check that we haven't tweeted this pet before.
+    history_file = ENV.fetch("history_file")
+    history_filepath = File.expand_path(history_file)
+    if File.file?(history_filepath)
+      num_tries = ENV.fetch("number_tries").to_i
+      history = File.readlines(history_filepath)
+      num_tries.times do
+        new_pet = true
+        history.each do |url|
+          if url.strip == pet[:link]
+            new_pet = false
+            break
+          end
+        end
+        break if new_pet
+        pet = fetch_pet
+      end
+    end
+
     if pet
+      File.open(history_filepath, "a") do |f|
+        f.puts pet[:link]
+      end
       message = TweetGenerator.create_message(pet[:name], pet[:description], pet[:link])
       TweetGenerator.tweet(message, pet[:pic])
     end
