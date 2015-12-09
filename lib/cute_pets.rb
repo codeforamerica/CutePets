86996feaa0de4a8f77722814ca12df1c7141aef6
Dillon Drobena <dillon.drobena@utdallas.edu>
2015-12-09 17:54:39 -0600

    Hopefully finished up facebook integration. Make code a bit cleaner.

--- lib/cute_pets.rb
+++ lib/cute_pets.rb
@@ -1,13 +1,12 @@
 require './lib/cute_pets/pet_fetcher'
-require './lib/cute_pets/tweet_generator'
-require './lib/cute_pets/status_generator'
+require './lib/cute_pets/post_generator'
 require 'dotenv'
 Dotenv.load
 
 module CutePets
   extend self
 
-  def post_pet_twitter()
+  def post_pet(environment)
     pet = nil
     if ENV.fetch('pet_datasource').downcase == 'petfinder'
       pet = PetFetcher.get_petfinder_pet
@@ -15,21 +14,15 @@ module CutePets
       pet = PetFetcher.get_petharbor_pet
     end
     if pet
-      message = TweetGenerator.create_message(pet[:name], pet[:description], pet[:link])
-      TweetGenerator.tweet(message, pet[:pic])
-    end
-  end
-
-  def post_pet_facebook()
-    pet = nil
-    if ENV.fetch('pet_datasource').downcase == 'petfinder'
-      pet = PetFetcher.get_petfinder_pet
-    else
-      pet = PetFetcher.get_petharbor_pet
-    end
-    if pet
-      message = StatusGenerator.create_message(pet[:name], pet[:description], pet[:link])
-      StatusGenerator.post(message, pet[:pic])
+      message = PostGenerator.create_message(pet[:name], pet[:description], pet[:link])
+      case environment
+        when :twitter
+          PostGenerator.post_twitter(message, pet[:pic])
+        when :facebook
+          PostGenerator.post_facebook(message, pet[:pic])
+        else
+          raise "Argument must be Facebook of Twitter"
+      end
     end
   end
 end
--- spec/facebook_post_generator_spec.rb
+++ spec/facebook_post_generator_spec.rb
@@ -1,7 +1,7 @@
 require 'cute_pets/post_generator'
 require 'minitest/autorun'
 
-describe 'TweetGenerator' do
+describe 'FacebookPostGenerator' do
   describe '.create_message' do
     it 'returns a string with the pet name, description and shelter link' do
       message = PostGenerator.create_message('Moofle', 'neutered female lab', 'http://www.example.org/moofle')
@@ -14,14 +14,14 @@ describe 'TweetGenerator' do
     end
   end
 
-  describe '.tweet' do
-    it 'uses the twitter gem to post a tweet' do
+  describe '.post' do
+    it 'uses the koala gem to post to facebook' do
       client_object = MiniTest::Mock.new
-      PostGenerator.stub :twitter_client, client_object do
+      PostGenerator.stub :facebook_client, client_object do
         message = 'Hi! I am Woofles. I am a neutered female nerdy cat. http://www.example.org/woofles'
         pet_pic = 'spec/fixtures/cat.jpg'
-        client_object.expect(:update_with_media, nil, [String, File])
-        PostGenerator.post_twitter(message, pet_pic)
+        client_object.expect(:put_connections, nil, [String, String, Hash])
+        PostGenerator.post_facebook(message, pet_pic)
       end
     end
   end
